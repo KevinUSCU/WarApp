ba8678588c4f13bbc63ac7bf4595ce73b63518e7
Kevin Springer <KevinUSCU@outlook.com>
2018-01-12 15:48:17 -0800

    Create basic war card game.

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var playerScore = 0
+    var cpuScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+        
+        if (leftRandomNumber > rightRandomNumber) {
+            playerScore += 1
+            leftScoreLabel.text = "\(playerScore)"
+        } else if (leftRandomNumber < rightRandomNumber) {
+            cpuScore += 1
+            rightScoreLabel.text = "\(cpuScore)"
+        }
+        
+        
+    }
+    
 }
 
